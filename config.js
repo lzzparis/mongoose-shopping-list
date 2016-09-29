98fba6b406bb0750d3f15e7d06acca7e7137c333
Lizzie Paris <lzz.paris@gmail.com>
2016-09-28 22:59:17 -0700

    update production URL to mLab

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-exports.DATABASE_URL  = process.env.DATABASE_URL ||
+exports.DATABASE_URL  =	process.env.DATABASE_URL ||
                         global.DATABASE_URL ||
                         (process.env.NODE_ENV === "production" ?
-                          "mongodb://localhost/shopping-list" :
+                          "mongodb://ds041566.mlab.com:41566/mongoose-shopping-list" :
                           "mongodb://localhost/shopping-list-dev");
 
 exports.PORT = process.env.PORT || 8080;
